35bc82fe06b1db052f5eec6dbd552e05375415b1
Pradeep Thapa <thapa.pradeepp@gmail.com>
2026-05-23 14:32:27 +0545

    feat: add dependency and sdk version
    - add hive_flutter for fast local database
    - add flutter_secure_storage for encrypted storage
    - add compile sdk, min sdk and ndk version in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.thapa.nexora"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,8 +24,8 @@ android {
         applicationId = "com.thapa.nexora"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
